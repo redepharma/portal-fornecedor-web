1eafe1aefe8d66b7750b5971d5984972125100ca
Henrique Gomes <oi.henriquegomes@gmail.com>
2025-07-02 11:11:40 -0300

    fix: corrige fallback de redirecionamento

--- shared/utils.ts
+++ shared/utils.ts
@@ -3,7 +3,7 @@ export function getApiBaseUrl(): string {
     const hostname = window.location.hostname;
 
     if (hostname.startsWith("10.7.0.116")) {
-      return process.env.NEXT_PUBLIC_API_BASE_URL || "http://10.7.0.116:3001";
+      return process.env.NEXT_PUBLIC_API_BASE_URL || "http://10.7.0.116:3004";
     }
 
     if (hostname === "fornecedor.nelfarma.dev.br") {
@@ -14,5 +14,11 @@ export function getApiBaseUrl(): string {
     }
   }
 
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://10.7.0.116:3001";
+  if (process.env.NEXT_PUBLIC_API_BASE_URL) {
+    return process.env.NEXT_PUBLIC_API_BASE_URL;
+  }
+
+  return process.env.NODE_ENV === "production"
+    ? "http://10.7.0.116:3004"
+    : "http://localhost:3004";
 }
