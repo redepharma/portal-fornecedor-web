eaea7ade4a884d4df22193964a60c436e556d59c
Henrique Gomes <oi.henriquegomes@gmail.com>
2025-07-02 09:00:32 -0300

    fix: corrige fallback de redirecionamento de apis

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,11 +1,9 @@
-import { env } from "process";
-
 export function getApiBaseUrl(): string {
   if (typeof window !== "undefined") {
     const hostname = window.location.hostname;
 
     if (hostname.startsWith("10.7.0.116")) {
-      return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+      return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3004";
     }
 
     if (hostname === "fornecedor.nelfarma.dev.br") {
@@ -16,5 +14,5 @@ export function getApiBaseUrl(): string {
     }
   }
 
-  return "http://localhost:3001";
+  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3004";
 }
